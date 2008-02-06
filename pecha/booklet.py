f9b8d3148e6857d18847c6de55ce1b6e7573765d
oubiwann <>
2008-02-06 08:11:44 +0000

    * Removed old funtion.

--- pecha/booklet.py
+++ pecha/booklet.py
@@ -1,39 +1,5 @@
 import math
 
-def printLayoutReport(totalPechaPages):
-    div, mod = divmod(totalPechaPages, 4)
-    if mod == 0:
-        totalActualPages = div
-    else:
-        totalActualPages = div + 1
-
-    for currentPage in xrange(1, totalPechaPages + 1):
-        if currentPage <= totalPechaPages / 2.0:
-            firstHalf = True
-        else:
-            firstHalf = False
-        secondHalf = not firstHalf
-        if firstHalf:
-            currentActualPage = int(currentPage / 2.0 + 0.5)
-        else:
-            currentActualPage = totalPechaPages - int(currentPage / 2.0 + 0.5)# - 3
-        if ((firstHalf and (currentPage % 2) == 1) or (secondHalf and (currentPage % 2) == 0)):
-            orientation = 'upsidedown'
-            side = '2nd'
-        else:
-            orientation = 'rightsideup'
-            side = '1st'
-        print currentPage, currentActualPage, side, orientation, firstHalf, secondHalf
-
-for tpp in xrange(1,8 + 1):
-    #printLayoutReport(tpp)
-    #print "\n"
-    pass
-
-def getOrientation():
-    pass
-
-
 # XXX move into pecha.base
 RIGHTSIDEUP = 0
 UPSIDEDOWN = 1
