0aa6de797ea1b25314dec8f692bf9eea0cafea22
oubiwann <>
2008-02-08 09:26:02 +0000

    * Added support for rotating blocks. * Moved cropping and rotating code into functions. * Updated TODO.

--- pecha/assembler.py
+++ pecha/assembler.py
@@ -2,6 +2,9 @@
 from pyPdf import PdfFileWriter, PdfFileReader
 from pyPdf.generic import NumberObject
 
+from pecha.base import UPSIDEDOWN
+from pecha.layouts.booklet import pechaFactory
+
 inch = 72.0
 pechaHeight = 8.5 * inch / 2
 pechaShortWidth = 11 * inch
@@ -12,15 +15,35 @@ def cropPDFPage(page, startX=0, startY=pechaHeight, endX=11*inch, endY=8.5*inch)
         page.mediaBox[index] = NumberObject(amount)
     return page
 
-inFile = 'test/sources/1perpage_test.pdf'
-text = PdfFileReader(file(inFile, "rb"))
-output = PdfFileWriter()
-count = text.getNumPages()
-for index in xrange(count):
-    page = cropPDFPage(text.getPage(index))
-    output.addPage(page)
+def _prepFiles(inFilename, outFilename):
+    """
+    """
+    source = PdfFileReader(file(inFilename, "rb"))
+    outPDF = PdfFileWriter()
+    outStream = file(outFilename, "wb")
+    return (source, outPDF, outStream)
 
-outName = 'crop.pdf'
-cropped = file(outName, "wb")
-output.write(cropped)
-cropped.close()
+def cropPDFFile(inFilename, outFilename):
+    """
+    """
+    text, output, cropped = _prepFiles(inFilename, outFilename)
+    for index in xrange(text.getNumPages()):
+        page = cropPDFPage(text.getPage(index))
+        output.addPage(page)
+    output.write(cropped)
+    cropped.close()
+
+def cropAndRotateFile(inFilename, outFilename):
+    text, output, cropped = _prepFiles(inFilename, outFilename)
+    pf = pechaFactory(text.getNumPages())
+    for index, block in enumerate(pf.getBlockList()):
+        page = cropPDFPage(text.getPage(index))
+        if block.orientation == UPSIDEDOWN:
+            page.rotateClockwise(180)
+        output.addPage(page)
+    output.write(cropped)
+    cropped.close()
+
+inFile = 'test/sources/1perpage_test.pdf'
+outFile = 'cropped.pdf'
+cropAndRotateFile(inFile, outFile)
