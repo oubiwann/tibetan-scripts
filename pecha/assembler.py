f5af35e3e837c013fa2912c415601d6ea4a301f5
oubiwann <>
2008-02-08 09:00:55 +0000

    2008.02.08
    * Added support for cropping PDF files.

--- pecha/assembler.py
+++ pecha/assembler.py
@@ -1,41 +1,26 @@
 #!/usr/bin/env python2.4
-import os
-from tempfile import mkstemp
-
-from reportlab.pdfgen.canvas import Canvas
-from reportlab.lib.pagesizes import legal, landscape
-from reportlab.lib.units import cm, mm, inch, pica
-
 from pyPdf import PdfFileWriter, PdfFileReader
+from pyPdf.generic import NumberObject
 
-pechaShort = (8.5*inch/2, 11*inch)
-pechaLong = (8.5*inch/2, 14*inch)
+inch = 72.0
+pechaHeight = 8.5 * inch / 2
+pechaShortWidth = 11 * inch
+pechaLongWidth = 14 * inch
 
-# http://www.reportlab.com/docs/userguide.pdf
-# Create the crop mark page
-#
-ign, tmpFile = mkstemp('.pdf', 'pecha_blank', '/tmp')
-cropMarks = Canvas(tmpFile, pagesize=landscape(pechaShort))
-cropMarks.showPage()
-cropMarks.save()
+def cropPDFPage(page, startX=0, startY=pechaHeight, endX=11*inch, endY=8.5*inch):
+    for index, amount in enumerate([startX, startY, endX, endY]):
+        page.mediaBox[index] = NumberObject(amount)
+    return page
 
-# http://pybrary.net/pyPdf/
-# test merging the pecha with the crop marks
-#
+inFile = 'test/sources/1perpage_test.pdf'
 text = PdfFileReader(file(inFile, "rb"))
-crop = PdfFileReader(file(tmpFile, "rb")).getPage(0)
 output = PdfFileWriter()
 count = text.getNumPages()
 for index in xrange(count):
-    if opts.pageLimit and count > opts.pageLimit:
-        break
-    page = text.getPage(index)
-    page.mergePage(crop)
+    page = cropPDFPage(text.getPage(index))
     output.addPage(page)
 
-outName = '%s_%s.pdf' % (origBase, opts.postfix)
-marked = file(outName, "wb")
-output.write(marked)
-marked.close()
-
-os.unlink(tmpFile)
+outName = 'crop.pdf'
+cropped = file(outName, "wb")
+output.write(cropped)
+cropped.close()
