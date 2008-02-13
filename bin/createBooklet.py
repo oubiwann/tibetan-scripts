c1c62f9f5ce6e2b2b935c3ffe1055dc48251f9a9
oubiwann <>
2008-02-13 22:45:16 +0000

    * Moved PDF processing from bin/addCropMarks.py to pecha.assembler. * Added test/sources. * Added script for creating booklets.

--- bin/createBooklet.py
+++ bin/createBooklet.py
@@ -2,21 +2,17 @@
 import os.path
 from optparse import OptionParser
 
-from pecha.assembler import addCropMarks
+from pecha.assembler import assembleBooklet
 
 usage = '''%prog [options] infile'''
 optp = OptionParser(usage=usage)
-optp.set_defaults(shade=0.75, suffix='marked', flip=False, pageLimit=None,
-                  drawTopCrop=False, drawBottomCrop=False, style='default',
-                  paperSize='legal')
-optp.add_option('-s', '--shade', dest='shade')
+optp.set_defaults(suffix='booklet', flip=False, pageLimit=None,
+                  paperSize='legal', orientation='landscape')
 optp.add_option('-p', '--paperSize', dest='paperSize')
+optp.add_option('-o', '--orientation', dest='orientation')
 optp.add_option('-u', '--suffix', dest='suffix')
 optp.add_option('-f', '--flip', dest='flip')
 optp.add_option('-n', '--pageLimit', dest='pageLimit')
-optp.add_option('-t', '--drawTopCrop', dest='drawTopCrop')
-optp.add_option('-b', '--drawBottomCrop', dest='drawBottomCrop')
-optp.add_option('-y', '--style', dest='style')
 (opts, args) = optp.parse_args()
 if not args:
     optp.error('You must pass the filename of the pdf to mark.')
@@ -26,5 +22,5 @@ inFile = args[0]
 origBase = os.path.splitext(inFile)[0]
 outFile = '%s_%s.pdf' % (origBase, opts.suffix)
 # Create the crop marked file
-addCropMarks(inFile, outFile, opts)
+assembleBooklet(inFile, outFile, opts)
 
--- pecha/assembler.py
+++ pecha/assembler.py
@@ -1,49 +1,171 @@
 #!/usr/bin/env python2.4
+from StringIO import StringIO
+
 from pyPdf import PdfFileWriter, PdfFileReader
 from pyPdf.generic import NumberObject
 
-from pecha.base import UPSIDEDOWN
-from pecha.layouts.booklet import pechaFactory
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib import pagesizes
+from reportlab.lib.pagesizes import legal, landscape
+
+from pecha.base import UPSIDEDOWN, FRONT, TOP
+from pecha.layouts import booklet
 
 inch = 72.0
 pechaHeight = 8.5 * inch / 2
 pechaShortWidth = 11 * inch
 pechaLongWidth = 14 * inch
 
-def cropPDFPage(page, startX=0, startY=pechaHeight, endX=11*inch, endY=8.5*inch):
-    for index, amount in enumerate([startX, startY, endX, endY]):
-        page.mediaBox[index] = NumberObject(amount)
-    return page
-
 def _prepFiles(inFilename, outFilename):
     """
     """
     source = PdfFileReader(file(inFilename, "rb"))
     outPDF = PdfFileWriter()
-    outStream = file(outFilename, "wb")
+    outStream = open(outFilename, "wb")
     return (source, outPDF, outStream)
 
-def cropPDFFile(inFilename, outFilename):
+def _invertBoxParams(params):
     """
     """
-    text, output, cropped = _prepFiles(inFilename, outFilename)
-    for index in xrange(text.getNumPages()):
-        page = cropPDFPage(text.getPage(index))
+    return [params[1], params[0], params[3], params[2]]
+
+def placeBlock(page, location):
+    """
+    """
+    # XXX this isn't working yet
+    if location == TOP:
+        x1, y1, x2, y2 = (0, 0, 11 * inch, 8.5 * inch)
+        page = cropPDFPage(page, x1, y1, x2, y2)
+    else:
+        x1, y1, x2, y2 = (0, pechaHeight, 11 * inch, pechaHeight + 8.5 * inch)
+        for index, amount in enumerate([x1, y1, x2, y2]):
+            page.artBox[index] = NumberObject(amount)
+    return page
+
+def cropPDFPage(page, startX=0, startY=pechaHeight, endX=14*inch, endY=8.5*inch):
+    """
+    """
+    for index, amount in enumerate([startX, startY, endX, endY]):
+        page.mediaBox[index] = NumberObject(amount)
+    return page
+
+def createMarkedPDF(filenameOrFH, opts):
+    """
+    """
+    # setup legths
+    length = opts.paperSize[1] / inch
+    if opts.style == 'default':
+        boxParamsMid = [0, 4, length, 0.5]
+    if opts.style == 'trim':
+        opts.drawTopCrop = True
+        opts.drawBottomCrop = True
+        boxParamsTop = [0, 0, length, 0.2]
+        boxParamsMid = [0, 3.85, length, 0.8]
+        boxParamsBot = [0, 8.3, length, 0.2]
+    # with PDF files that have been converted from PS files via the command line
+    # tool pstopdf, we've found that we need to invert the crop box dimensions
+    if opts.flip:
+        boxParamsMid = _invertBoxParams(boxParamsMid)
+        if opts.drawTopCrop:
+            boxParamsTop = _invertBoxParams(boxParamsTop)
+        if opts.drawBottomCrop:
+            boxParamsBot = _invertBoxParams(boxParamsBot)
+    # now create the PDF page
+    c = Canvas(filenameOrFH, pagesize=landscape(opts.paperSize))
+    c.setStrokeColorRGB(*opts.shade)
+    c.setFillColorRGB(*opts.shade)
+    c.rect(*[inch*x for x in boxParamsMid], **{'fill': 1})
+    if opts.drawTopCrop:
+        c.rect(*[inch*x for x in boxParamsTop], **{'fill': 1})
+    if opts.drawBottomCrop:
+        c.rect(*[inch*x for x in boxParamsBot], **{'fill': 1})
+    c.showPage()
+    c.save()
+    return (filenameOrFH, c)
+
+def createBlankPDF(filenameOrFH, opts):
+    """
+    """
+    orientation = getattr(pagesizes, opts.orientation)
+    c = Canvas(filenameOrFH, pagesize=orientation(opts.paperSize))
+    c.showPage()
+    c.save()
+    return (filenameOrFH, c)
+
+def addCropMarks(inFilename, outFilename, opts):
+    """
+    """
+    # setup color and sizes
+    opts.shade = [float(opts.shade)] * 3
+    opts.paperSize = getattr(pagesizes, opts.paperSize)
+    crop, canvas = createMarkedPDF(StringIO(), opts)
+    markPage = PdfFileReader(crop).getPage(0)
+    source, output, marked = _prepFiles(inFilename, outFilename)
+    for index in xrange(source.getNumPages()):
+        page = source.getPage(index)
+        page.mergePage(markPage)
+        output.addPage(page)
+    output.write(marked)
+    marked.close()
+    crop.close()
+
+def cropPDFFile(inFilename, outFilename, opts=None):
+    """
+    """
+    opts.paperSize = getattr(pagesizes, opts.paperSize)
+    source, output, cropped = _prepFiles(inFilename, outFilename)
+    for index in xrange(source.getNumPages()):
+        params = (
+            source.getPage(index), 0, pechaHeight,
+            float(opts.paperSize[1]), float(opts.paperSize[0]))
+        page = cropPDFPage(*params)
         output.addPage(page)
     output.write(cropped)
     cropped.close()
 
-def cropAndRotateFile(inFilename, outFilename):
-    text, output, cropped = _prepFiles(inFilename, outFilename)
-    pf = pechaFactory(text.getNumPages())
+def cropAndRotateFile(inFilename, outFilename, opts=None):
+    """
+    """
+    source, output, cropped = _prepFiles(inFilename, outFilename)
+    pf = booklet.pechaFactory(source.getNumPages())
     for index, block in enumerate(pf.getBlockList()):
-        page = cropPDFPage(text.getPage(index))
+        page = cropPDFPage(source.getPage(index))
         if block.orientation == UPSIDEDOWN:
             page.rotateClockwise(180)
         output.addPage(page)
     output.write(cropped)
     cropped.close()
 
-inFile = 'test/sources/1perpage_test.pdf'
-outFile = 'cropped.pdf'
-cropAndRotateFile(inFile, outFile)
+def assembleBooklet(inFilename, outFilename, opts):
+    """
+    """
+    opts.paperSize = getattr(pagesizes, opts.paperSize)
+    blank, canvas = createBlankPDF(StringIO(), opts)
+    source, output, assembled = _prepFiles(inFilename, outFilename)
+    pf = booklet.pechaFactory(source.getNumPages())
+    for sheet in pf.sheets:
+        page = side = None
+        for block in sheet.blocks:
+            if side != block.side:
+                if page != None:
+                    # finish up the old page
+                    output.addPage(page)
+                # make new PDF page
+                page = PdfFileReader(blank).getPage(0)
+            side = block.side
+            blockPDF = source.getPage(block.number - 1)
+            # crop the page to pecha size
+            params = (
+                source.getPage(index), 0, pechaHeight,
+                float(opts.paperSize[1]), float(opts.paperSize[0]))
+            blockPDF = cropPDFPage(*params)
+            # rotate if need be
+            if block.orientation == UPSIDEDOWN:
+                blockPDF.rotateClockwise(180)
+            # figure out where on the page to put it
+            blockPDF = placeBlock(blockPDF, block.location)
+            page.mergePage(blockPDF)
+    output.write(assembled)
+    assembled.close()
+    blank.close()
+
