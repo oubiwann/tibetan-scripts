84a955db8e34e2712728b8c21a0da84e6c30c37b
oubiwann <>
2008-02-23 19:27:16 +0000

    * Added code to stubbed unit tests.

--- pecha/util.py
+++ pecha/util.py
@@ -1,22 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import re
+
 def toUpper(string):
     """
     >>> data = ''' Sang gyè
     ...  chag dü
     ...  chö kyi'''
     >>> print toUpper(data)
+     SANG GYÈ
+     CHAG DÜ
+     CHÖ KYI
     """
-    return string
+    # è toÈ 
+    string = string.replace('\xc3\xa8', '\xc3\x88')
+    # ü to Ü
+    string = string.replace('\xc3\xbc', '\xc3\x9c')
+    # ö toÖ
+    string = string.replace('\xc3\xb6', '\xc3\x96')
+    return string.upper()
 
 
 def normalizeVerseBreak(string):
     """
-    >>> data= '  CHANG CHUB SEM CHOG RIN PO CHE'
+    >>> data = '  CHANG CHUB SEM CHOG RIN PO CHE'
     >>> normalizeVerseBreak(data)
-    CHANG CHUB SEM CHOG RIN PO CHE
-    >>> data= 'CHANG CHUB SEM CHOG RIN PO CHE/ MA KYE PA NAM KYE GUYR CHIG'
+    'CHANG CHUB SEM CHOG RIN PO CHE'
+    >>> data = 'CHANG CHUB SEM CHOG RIN PO CHE /'
     >>> normalizeVerseBreak(data)
-    CHANG CHUB SEM CHOG RIN PO CHE   MA KYE PA NAM KYE GUYR CHIG
+    'CHANG CHUB SEM CHOG RIN PO CHE    '
+    >>> data = 'CHANG CHUB SEM CHOG RIN PO CHE/ MA KYE PA NAM KYE GUYR CHIG'
+    >>> normalizeVerseBreak(data)
+    'CHANG CHUB SEM CHOG RIN PO CHE    MA KYE PA NAM KYE GUYR CHIG'
     """
+    string = string.strip()
+    string = string.replace('/', ' / ')
+    string = re.sub('\s+', ' ', string)
+    string = string.replace('/', '  ')
     return string
 
 def _test():
