63755e52d07f042bfa3cc838d7e6aa68c7c50683
Duncan McGreggor <oubiwann@lorien>
2009-07-25 14:40:12 -0500

    ++ PechaCreation/trunk/ChangeLog	2009-07-24 22:14:00 +0000 * Added line markers to transliteration output. * Added a debug option for the transliteration script, including line numbers and run report.

--- bin/upperTranslit.py
+++ bin/upperTranslit.py
@@ -2,14 +2,26 @@
 Convert a lower-case transliterated file to an upper-case transliterated file.
 
 Usage:
-    python %s <unicode file>
+    python %prog <unicode input file> [<output file>]
 
 where the unicode file contains lower-case Tibetan transliteration text.
 """
 import sys
+from optparse import OptionParser
 from subprocess import Popen, PIPE
 
-def main(filename):
+
+def main():
+    parser = OptionParser(usage=__doc__)
+    parser.set_defaults(debug=False)
+    parser.add_option(
+        "-d", "--debug", action="store_true", dest="debug",
+        help="")
+    (options, args) = parser.parse_args()
+    filename = args[0]
+    outputFilename = None
+    if len(args) > 1:
+        outputFilename = args[1]
     command = [
         "perl",
         "third-party/Lingua-BO-Wylie-dev/bin/pronounce.pl", 
@@ -18,17 +30,32 @@ def main(filename):
         "-",
         ]
     output = Popen(command, stdout=PIPE).communicate()[0]
-    for line in output.decode("utf-8").upper().split("\n"):
+    lines = output.decode("utf-8").upper().split("\n")
+    badlines = []
+    data = ""
+    for lineNumber, line in enumerate(lines):
+        lineNumber += 1
         if len(line) > 0:
             line = "%s /  " % line
-        print line
+        if options.debug:
+            line = "%s  %s" % (str(lineNumber).rjust(6, " "), line)
+            if "?" in line:
+                badlines.append(line)
+        if outputFilename and not options.debug:
+            data += line + "\n"
 
+        else:
+            print line
+        if data:
+            fh = open(outputFilename, "w+")
+            fh.write(data.encode("utf-8"))
+            fh.close()
+    if options.debug and badlines:
+        print "\nThe following lines have ambiguous words (marked"
+        print "by question marks):\n"
+        for line in badlines:
+            print line
+        print "\n"
 
 if __name__ == "__main__":
-    script = sys.argv[0]
-    try:
-        filename = sys.argv[1]
-    except IndexError:
-        print __doc__ % script
-    else:
-        main(filename)
+    main()
