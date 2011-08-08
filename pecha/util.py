8ecf5518a50689baff3a76c42f0161d0971fd85f
Duncan McGreggor <duncan@ubuntu.com>
2011-08-07 23:17:25 -0600

    Fixed normalization.

--- pecha/util.py
+++ pecha/util.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
+
 import re
+
 
 def toUpper(string):
     """
@@ -12,11 +14,11 @@ def toUpper(string):
      CHAG DÜ
      CHÖ KYI
     """
-    # è toÈ 
+    # è to È 
     string = string.replace('\xc3\xa8', '\xc3\x88')
     # ü to Ü
     string = string.replace('\xc3\xbc', '\xc3\x9c')
-    # ö toÖ
+    # ö to Ö
     string = string.replace('\xc3\xb6', '\xc3\x96')
     return string.upper()
 
@@ -28,20 +30,27 @@ def normalizeVerseBreak(string):
     'CHANG CHUB SEM CHOG RIN PO CHE'
     >>> data = 'CHANG CHUB SEM CHOG RIN PO CHE /'
     >>> normalizeVerseBreak(data)
-    'CHANG CHUB SEM CHOG RIN PO CHE    '
-    >>> data = 'CHANG CHUB SEM CHOG RIN PO CHE/ MA KYE PA NAM KYE GUYR CHIG'
+    'CHANG CHUB SEM CHOG RIN PO CHE /  '
+    >>> data = 'CHANG CHUB SEM CHOG RIN PO CHE/'
     >>> normalizeVerseBreak(data)
-    'CHANG CHUB SEM CHOG RIN PO CHE    MA KYE PA NAM KYE GUYR CHIG'
+    'CHANG CHUB SEM CHOG RIN PO CHE /  '
+    >>> data = 'CHANG CHUB SEM CHOG RIN PO CHE   /   '
+    >>> normalizeVerseBreak(data)
+    'CHANG CHUB SEM CHOG RIN PO CHE /  '
+    >>> data = 'CHANG CHUB SEM CHOG RIN PO CHE/MA KYE PA NAM KYE GUYR CHIG/'
+    >>> normalizeVerseBreak(data)
+    'CHANG CHUB SEM CHOG RIN PO CHE /  MA KYE PA NAM KYE GUYR CHIG /  '
     """
     string = string.strip()
-    string = string.replace('/', ' / ')
-    string = re.sub('\s+', ' ', string)
-    string = string.replace('/', '  ')
+    string = re.sub('\s+/\s?', '/', string)
+    string = string.replace('/', ' /  ')
     return string
+
 
 def _test():
     import doctest
     doctest.testmod()
+
 
 if __name__ == '__main__':
     _test()
